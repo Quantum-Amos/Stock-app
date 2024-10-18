14702ff0f287b80ad4f7b4e0172c0d6e4d01b0d2
quantum-nyame <skonadu@quantumgroupgh.com>
2024-10-18 09:40:51 +0000

    Fix UI loader

--- src/stores/purchase.ts
+++ src/stores/purchase.ts
@@ -1,19 +1,22 @@
 import { getRequestHandler } from "@/utils/httpHandler";
 import { defineStore } from "pinia";
+import { useUiStore } from "./ui";
 
 export const usePurchaseStore = defineStore('Purchase Orders', () => {
     const purchaseItems = ref<any>([])
+    const uiStore = useUiStore()
     const orderTypes = ref<any>()
 
     const getOrderType = async() => {
         await getRequestHandler(`/purchase-order-types`, true)
         .then((res) => {
             orderTypes.value = res
+            uiStore.loading = false
         })
         .catch((error) => {
             console.error(error)
         })
     }
 
-    return { getOrderType, purchaseItems, orderTypes }
+    return { getOrderType, purchaseItems, orderTypes     }
 })
